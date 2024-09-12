e03e5f40e8b8c5da0f7e7c62b539ddd46b15fe32
unknown <hueouts@gmail.com>
2024-09-12 05:57:45 -0400

    final commit

--- src/component/Signup.js
+++ src/component/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom';
 import axios from 'axios'
-
+// make it in old practice 
 export default function Signup() {
   const history =useHistory();
   const s_up= async (e)=>{
